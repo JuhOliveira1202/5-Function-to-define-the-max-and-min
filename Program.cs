ff15e2c24850704e6a046db099bc618d6dcd59b3
Juliana Oliveira <134332673+JuhOliveira1202@users.noreply.github.com>
2023-07-17 12:23:02 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+class Program
+{
+
+    public static int LerNumero()
+    {
+        return Convert.ToInt32(Console.ReadLine());
+    }
+
+    public static int LerInteiro(int min, int max)
+    {
+        int x;
+
+        do
+        {
+            Console.WriteLine("Insira um número");
+            x = Convert.ToInt32(Console.ReadLine());
+
+            if (x < min || x > max)
+            {
+                Console.WriteLine("O número não é válido");
+            }
+        }
+        while (x < min || x > max);
+        return x;
+
+    }
+
+    static void Main(string[] args)
+    {
+
+        int minimo, maximo;
+
+        Console.WriteLine("Defina o mínimo");
+        minimo = LerNumero();
+        Console.WriteLine("Defina o máximo");
+        maximo = LerNumero();
+
+        LerInteiro(minimo, maximo);
+    }
+}
